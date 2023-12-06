0985452be0536a1dd6b087f60258827f45e5faf8
Jimena Gensollen <jimenagensollen@gmail.com>
2023-12-06 15:34:05 -0600

    added selectedDay
    initial status: today's date
    on press any day that will become the selectedDay and appear in the middle of the calendar with a different color

--- reactNative-elements/components/Calendar/Calendar.js
+++ reactNative-elements/components/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { View, Text, FlatList } from "react-native";
 // import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import Day from "./Day";
@@ -6,38 +6,72 @@ import moment, {duration, subtract} from "moment";
 
 const Calendar = () => {
 
+  const flatListRef = useRef();
   const today = moment();
-  const threeMonthsBeforeToday =  moment().subtract(3, 'months');
-  const threeMonthsAfterToday = moment().add(3, 'months');
+  const [ selectedDay, setSelectedDay ] = useState(21);
+  const threeWeeksBeforeToday =  moment().subtract(3, 'weeks');
+  const threeWeeksAfterToday = moment().add(3, 'weeks');
   
-  const getListDate = () => {
+  const getListDate = (selected) => {
     let dateList = [];
-    let current = threeMonthsBeforeToday;
+    let current = threeWeeksBeforeToday;
     let id = 0
-
-    while (current.toString().slice(0,10) !== threeMonthsAfterToday.toString().slice(0,10)) {
+    while (current.toString().slice(0,10) !== threeWeeksAfterToday.toString().slice(0,10)) {
       dateList.push({id:id, date: current.toString().slice(0,10)});
       id = id+1;
       current = current.add(1, 'day');
     }
-
-    return dateList
-
+    if (selected == undefined) {
+      if (today !== undefined) {
+        let currentSelectedDay = dateList.filter((date)=> {
+          if ( date.date == today.toString().slice(0,10)) return true
+        })[0];
+        setSelectedDay(currentSelectedDay.id)
+      } 
+    } 
+    return dateList;
+    
   }
 
-  const [ dateList, setDateList ] = useState(getListDate())
+  const [ currentDateList, setCurrentDateList ] = useState([]);
+
+  useEffect(()=>{
+    if (currentDateList.length == 0 ) {
+      setCurrentDateList(getListDate(selectedDay))
+    } else {
+      flatListRef.current.scrollToIndex({index: selectedDay, animated: true, viewPosition: 0.5}) 
+    }
+  })
+
+  useEffect (()=> {
+    if (flatListRef.current && selectedDay !== undefined && currentDateList.length > 0) {
+      flatListRef.current.scrollToIndex({index: selectedDay, animated: true, viewPosition: 0.5}) 
+    }
+  }, [selectedDay])
+
 
   return (
     <View style={{ }}>
-      
       <FlatList
+      ref={flatListRef}
       horizontal={true}
-      data={dateList}
+      data={currentDateList}
+      onScrollToIndexFailed={(error)=>{
+        const index = error?.index? error.index : 0;
+        setTimeout(()=>flatListRef.current.scrollToIndex({index:index, animated: true, viewPosition: 0.5}, 200))
+        }
+      }
       style={{margin: 20, alignSelf: 'flex-start',borderWidth: 0.5, }}
-      renderItem={({item}) => {
+      renderItem={({item, index}) => {
+        if (index==selectedDay) flatListRef.current.scrollToIndex({index: selectedDay, animated: true, viewPosition: 0.5})
+
         return(
           <Day
+          index={index}
             date={item.date}
+            selectedDay={selectedDay}
+            setSelectedDay={(state)=> setSelectedDay(state)}
+            isSelected={index==selectedDay}
             />
           )
         }}
--- reactNative-elements/components/Calendar/Day.js
+++ reactNative-elements/components/Calendar/Day.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity} from "react-native";
 
-const Day = ({date}) => {
-
+const Day = ({date, selectedDay, setSelectedDay, isSelected, index}) => {
   return (
-    <View style={{borderWidth: 0.5, padding: 8, fontSize: 18, height: 'auto', minWidth: 45}}>
+    <TouchableOpacity onPress={()=>setSelectedDay(index)} style={{borderWidth: 0.5, padding: 8, fontSize: 18, height: 'auto', minWidth: 45, }}>
       <View style={{alignItems: 'center'}}>
-      <Text>
+      <Text style={{color: isSelected ? 'blue' : 'black'}}>
         {date.slice(0,3)}
       </Text>
-      <Text>
+      <Text style={{color: isSelected ? 'blue' : 'black'}}>
         {date.slice(8)}
       </Text>
+      <Text style={{color: isSelected ? 'blue' : 'black'}}>
+        {date.slice(4,7)}
+      </Text>
       </View>
-    </View>
+    </TouchableOpacity>
   )
 }
 
